fb69a3ad548f1aff7986e027c685d38a7c45fd29
Artur Augusto Martins <arturaugusto@gmail.com>
2015-09-10 06:34:56 -0300

    Captcha key change

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6LcAiPUSAAAAADCTjodUlSiBfyD8qTEc-44lT_lu'
-  config.private_key = '6LcAiPUSAAAAAJJCVS50yONnUpqn7QIKIzPGBcmL'
-  #config.proxy = 'http://myproxy.com.au:8080'
-end
+#Recaptcha.configure do |config|
+#  config.public_key  = '6LcAiPUSAAAAADCTjodUlSiBfyD8qTEc-44lT_lu'
+#  config.private_key = '6LcAiPUSAAAAAJJCVS50yONnUpqn7QIKIzPGBcmL'
+#  #config.proxy = 'http://myproxy.com.au:8080'
+#end
