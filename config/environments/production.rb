a9b62b63a6ec54135f28a97cdee83e827b6aa45e
Artur Augusto Martins <arturaugusto@gmail.com>
2015-09-13 17:36:48 -0300

    try to fix fonts on pdf 5

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,14 +24,14 @@ MeasWeb::Application.configure do
 
   # set true Needed when using wiked_pdf??
   config.serve_static_assets = true
-  config.assets.precompile += ['bootstrap-overwrite.css.scss']
+  config.assets.precompile += ['pdf.css']
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
