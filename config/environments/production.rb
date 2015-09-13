11019bffcbe770b0c088337d7165a29ddad53993
Artur Augusto Martins <arturaugusto@gmail.com>
2015-09-13 17:42:59 -0300

    clean back trials. No sucess

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,15 +23,14 @@ MeasWeb::Application.configure do
   #config.serve_static_assets = false
 
   # set true Needed when using wiked_pdf??
-  config.serve_static_assets = true
-  config.assets.precompile += ['pdf.css']
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
